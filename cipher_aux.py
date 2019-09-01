bf3f912bad725e88e842f9598befedf1e5291710
kali <kaliregenold@yahoo.com>
2019-09-01 10:58:05 -0600

    Hard code valid alpha and fix case inconsistency.

--- cipher_aux.py
+++ cipher_aux.py
@@ -15,4 +15,7 @@ def clean_list(message):
     return string_to_list(s)
 
 def mod_char(a):
-    return ((a - ord('a')) % 26) + ord('a')
+    return ((a - ord('A')) % 26) + ord('A')
+
+VALID_ALPHA     = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
+VALID_ALPHA_INV = [1, 9, 21, 15, 3, 19, 7, 23, 11, 5, 17, 25]
