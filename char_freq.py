5305d36d38af33a49d0249b5817e90ed9472350c
kali <kaliregenold@yahoo.com>
2019-09-24 20:05:14 -0600

    Update char_freq to be compatible with other scripts.

--- char_freq.py
+++ char_freq.py
@@ -1,38 +1,32 @@
-import re
+#!/bin/python3
+
 import sys
-
-BASE_A = 65
-
-# Remove anything not an alpha character
-# and capitalize
-def clean_text(text):
-    regex = re.compile('[^a-zA-Z]')
-    return regex.sub('', text).upper()
+from cipher_aux import *
 
 # Return the frequency that each character
 # occurs in the text string
-def char_freq(text):
+def char_freq(message_l):
     char_count = [0]*26
-    c_text = clean_text(text)
-    for c in c_text:
-        char_count[ord(c)-BASE_A] += 1
-    total_count = len(c_text)
+    for c in message_l:
+        char_count[c] += 1
+    total_count = len(message_l)
     return [x/total_count for x in char_count]
 
 # Print the character frequencies nicely
 def print_freq(freq):
     for i in range(0, len(freq)):
-        print(chr(i+BASE_A), "%.3f" % freq[i])
+        print(chr(i+ord('A')), "%.3f" % freq[i])
 
 # Prints freqencies of each character in given
 # text file
 def main():
     if len(sys.argv) != 2:
-        print("python3 word_freq.py [text_file_path]")
+        print("python3 char_freq.py [text_file_path]")
 
     text_file = open(sys.argv[1], 'r')
     text = text_file.read()
-    freq = char_freq(text)
+    message_l = string_to_list(clean_string(text))
+    freq = char_freq(message_l)
     print_freq(freq)
 
 if __name__ == "__main__":
